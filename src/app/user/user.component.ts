c483580ec1dbd9b15d062938dc84c3ccd9fe157f
Mark Hegedus <hegeee23@hotmail.comgit config --global user.email hegeee23@hotmail.com>
2024-07-05 19:24:32 +0200

    work on database

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTooltipModule} from '@angular/material/tooltip';
@@ -6,6 +6,8 @@ import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
 import {MatCardModule} from '@angular/material/card';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { onSnapshot } from "firebase/firestore";
 
 @Component({
   selector: 'app-user',
@@ -15,13 +17,44 @@ import {MatCardModule} from '@angular/material/card';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-
+  unsubUsers;
+  firestore: Firestore = inject(Firestore);
   user: User = new User();
+  usersList: User[] = []
   
   constructor(public dialog: MatDialog){
+    this.unsubUsers = this.subUsers();
+  }
+
+  setUserObject(obj: any): User {
+    return new User({
+        firstName: obj.firstName || "",
+        lastName: obj.lastName || "",
+        birthDate: obj.birthDate || "",
+        street: obj.street || "",
+        zipCode: obj.zipCode || "",
+        city: obj.city || ""
+    });
+}
+
+subUsers() {
+    return onSnapshot(this.getUsersRef(), (users) => {
+        this.usersList = [];
+        users.forEach(element => {
+            this.usersList.push(this.setUserObject(element.data()));
+        });
+    });
+}
+
+  ngonDestroy(){
+     this.unsubUsers();
   }
 
   openDialog(){
     this.dialog.open(DialogAddUserComponent)
+  }
+
+  getUsersRef(){
+    return collection(this.firestore, 'users')
   }
 }
